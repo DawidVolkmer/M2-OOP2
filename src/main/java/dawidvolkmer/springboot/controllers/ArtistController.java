23a76b98500b8ba06ee44e30dbe195cb62edcbb5
Dawid Volkmer <dvolkmer@INZYNIERIA.LOCAL>
2020-05-16 13:38:55 +0200

    Add new files

---
+++ src/main/java/dawidvolkmer/springboot/controllers/ArtistController.java
@@ -0,0 +1,25 @@
+package dawidvolkmer.springboot.controllers;
+
+import dawidvolkmer.springboot.repositories.ArtistRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class ArtistController {
+
+    private ArtistRepository artistRepository;
+
+    public ArtistController(ArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+
+    @RequestMapping("/artists")
+    public String getArtists(Model model) {
+
+        model.addAttribute("artists", artistRepository.findAll());
+
+        return "artists";
+    }
+}
